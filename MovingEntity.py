dc0f30a37d934964050724be777c73cdb37c87dd
GabrielSau <133013295+GabrielSau@users.noreply.github.com>
2026-01-17 15:30:34 +0100

    Finished class MovingEntity

--- MovingEntity.py
+++ MovingEntity.py
@@ -1,30 +1,30 @@
-import Entity
+from Entity import Entity
 
 class MovingEntity(Entity):
     CELL_SIZE = 20
-    DEFAULT_SPEED = 10
+    DEFAULT_SPEED = 8
+
     def __init__(self):
+        """Initialize the moving entity"""
         super().__init__()
         self._dx = self.CELL_SIZE
         self._dy = 0
-        self._speed = self.DEFAULT_SPEED
-        self._cell = self.CELL_SIZE
 
     def set_direction(self, dx: int, dy: int):
         """
-        Set the direction of the entity.
+        Set the direction of the entity
         
         Args:
-            dx: Change in x direction.
-            dy: Change in y direction.
+            dx: Change in x direction
+            dy: Change in y direction
         """
-        self._dx += dx
-        self._dy += dy
+        self._dx = dx
+        self._dy = dy
         
 
     def set_default_speed(self, value:int):
         """
-        Set the default speed of the entity.
+        Set the default speed of the entity
         """
         if value > 0:
             self._speed = value
@@ -34,7 +34,7 @@ class MovingEntity(Entity):
         Set the cell size
         
         Args:
-            value: An integer representing the cell size.
+            value: An integer representing the cell size
         """
         if value > 0:
             self._cell = value
